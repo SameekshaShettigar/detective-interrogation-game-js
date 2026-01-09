a7e4464e16dfeaeff23ffe1228da25af275cf3f1
Sameeksha <164968851+SameekshaShettigar@users.noreply.github.com>
2026-01-09 20:06:48 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,30 @@
+const suspects = [
+  {
+    name: "Alex",
+    guilty: false,
+    answers: [
+      "I was at home watching TV.",
+      "I barely knew the victim.",
+      "I have nothing to hide."
+    ]
+  },
+  {
+    name: "Blake",
+    guilty: true,
+    answers: [
+      "I was out... just driving.",
+      "We had some arguments.",
+      "Trust? That's your job, detective."
+    ]
+  },
+  {
+    name: "Casey",
+    guilty: false,
+    answers: [
+      "At work, late shift.",
+      "Yes, we were friends.",
+      "Check my records."
+    ]
+  }
+];
 
